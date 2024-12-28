91bf70bbdd6e6fe0e5cc56fe3b64c66157bcbab0
abhix201 <abhix201@gmail.com>
2024-12-28 11:52:06 +0530

    database connection with vercel

---
+++ .next/server/app/query/route.js
@@ -0,0 +1,7 @@
+const CHUNK_PUBLIC_PATH = "server/app/query/route.js";
+const runtime = require("../../chunks/[turbopack]_runtime.js");
+runtime.loadChunk("server/chunks/node_modules_75939d._.js");
+runtime.loadChunk("server/chunks/[root of the server]__3468ee._.js");
+runtime.loadChunk("server/chunks/_480df2._.js");
+runtime.getOrInstantiateRuntimeModule("[project]/.next-internal/server/app/query/route/actions.js [app-rsc] (ecmascript)", CHUNK_PUBLIC_PATH);
+module.exports = runtime.getOrInstantiateRuntimeModule("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/app/query/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)", CHUNK_PUBLIC_PATH).exports;
